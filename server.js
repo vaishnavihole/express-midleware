b74daa8a181e59303b521b0481261c54829f4c8c
vaishnavihole <vaishnavihole1@gmail.com>
2023-03-15 12:08:24 +0530

    use logger function

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require('express')
 const app = express()
 
+app.use(logger)
+
 app.get('/', (req, res) => {
+    console.log('Home Page');
     res.send('Home Page')
 })
 
 
 app.get('/users', (req, res) => {
+     console.log('User Page');
     res.send('User Page')
 })
 
 function logger(req, res, next) {
-    console.log();
+    console.log('log');
+    next()
 }
 
 app.listen(3000)
