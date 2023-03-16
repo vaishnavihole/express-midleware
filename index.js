f3f69f1073b16132da9406ca31225c135c7e17c9
vaishnavihole <vaishnavihole1@gmail.com>
2023-03-16 08:24:15 +0530

    use the function in api

--- index.js
+++ index.js
@@ -5,8 +5,22 @@ app.use(express.json())
 
 const PORT = 5000;
 
+const simpleFunction = (req, res, next)=>{
+    const token = req.body.token || req.query.token;
+    if(token==="Vaishu"){
+        next();
+    }
+    else{
+        return res.json({
+            success: false,
+            data:null,
+            message: "Token is incorrect"
+        })
+    }
+}
 
-app.get('/rooms', (req, res)=>{
+
+app.get('/rooms', simpleFunction, (req, res)=>{
     const rooms = "Room Data";
 
     res.json({
