31018be76ee5d3e254a310a6f264dd4bc5278563
vaishnavihole <vaishnavihole1@gmail.com>
2023-03-16 08:19:49 +0530

    normal get api added

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ app.use(express.json())
 const PORT = 5000;
 
 
+app.get('/rooms', (req, res)=>{
+    const rooms = "Room Data";
+
+    res.json({
+        success: true,
+        data:rooms,
+        message: "Room fetched successfully"
+    })
+})
+
 
 app.listen(PORT, ()=>{
 console.log(`Server started on Port ${PORT}`)
